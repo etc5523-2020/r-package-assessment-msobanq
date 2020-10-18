8d67fa443ca350c3437090362eb45e426540f5dd
msoban <m.soban84@gmail.com>
2020-10-19 03:23:46 +1100

    added documentation

---
+++ R/covid_data.R
@@ -0,0 +1,16 @@
+#' World Covid-19 Data
+#' 
+#' This dataset countains covid-19 data on all countries.
+#' 
+#' TODO: add description
+#' 
+#' @source 
+#' The data below was obtained from https://ourworldindata.org/coronavirus
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+"covid_data"
+ 
