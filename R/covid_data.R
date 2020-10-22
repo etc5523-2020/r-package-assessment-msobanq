2f1ca4fa67c1d4dfce6fa8cf2b0d2c883e94d516
msoban <m.soban84@gmail.com>
2020-10-22 23:12:47 +1100

    added infor on the data set

--- R/covid_data.R
+++ R/covid_data.R
@@ -2,7 +2,7 @@
 #' 
 #' This dataset countains covid-19 data on **all** countries.
 #' 
-#' TODO: add description
+#' @description the data is used in the app and is extensive, it not only focuses on the status of corona virus cases but also gives data on other factors that might be contributing to the pandemic.
 #' 
 #' @source 
 #' The data below was obtained from https://ourworldindata.org/coronavirus
